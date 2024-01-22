c9a6ad7b27a1c4ebdbb322d110fd7ce550deaf56
Aleksandr <Alfa>
2024-01-22 22:23:47 +0300

    add functions PrintArray, CreateArray

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
-﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
-// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
-// либо задать на старте выполнения алгоритма. 
-// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+﻿void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+            Console.Write($"{array[i]}");
+        else Console.Write($"{array[i]}" + ", ");
+    }
+    Console.Write("]");
+}
 
+string[] CreateArray()
+{
+    Console.WriteLine("Введите количество слов: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите слово: ");
+    string[] array = new string[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
